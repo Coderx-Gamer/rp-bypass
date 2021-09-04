b42e658934dbb11de8a1194703e378313e775591
Coderx-Gamer <85349822+Coderx-Gamer@users.noreply.github.com>
2021-09-04 08:35:20 -0400

    Delete Main.java

--- src/main/java/net/rpbypass/Main.java
+++
@@ -1,9 +0,0 @@
-package net.rpbypass;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class Main implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
-    }
-}
