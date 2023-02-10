830a4db15a04e2cdeae94e373b9d4f27db0d65a0
David <85349822+Coderx-Gamer@users.noreply.github.com>
2023-02-10 12:05:46 -0500

    Add files via upload

--- src/main/java/net/rpbypass/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/net/rpbypass/mixin/ClientPlayNetworkHandlerMixin.java
@@ -4,26 +4,25 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.packet.c2s.play.ResourcePackStatusC2SPacket;
 import net.minecraft.network.packet.s2c.play.ResourcePackSendS2CPacket;
-import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static net.rpbypass.SharedVariables.forceDeny;
-import static net.rpbypass.SharedVariables.resourcePackBypass;
+import java.util.regex.Pattern;
+
+import static net.rpbypass.SharedVariables.*;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public class ClientPlayNetworkHandlerMixin {
-
     private static final MinecraftClient CLIENT = MinecraftClient.getInstance();
 
     @Inject(at = @At("HEAD"), method = "onResourcePackSend", cancellable = true)
     public void onResourcePackSend(ResourcePackSendS2CPacket packet, CallbackInfo ci) {
         if (resourcePackBypass && packet.isRequired()) {
             CLIENT.getNetworkHandler().sendPacket(new ResourcePackStatusC2SPacket(ResourcePackStatusC2SPacket.Status.ACCEPTED));
-            log("This server has a required resource pack.");
+            log("This server has a required resource pack, message from server: " + textCompoundToString(packet.getPrompt() == null ? Text.of("(none)") : Text.of("\"" + textCompoundToString(packet.getPrompt()) + "\"")));
             ci.cancel();
         } else if (forceDeny && resourcePackBypass) {
             CLIENT.getNetworkHandler().sendPacket(new ResourcePackStatusC2SPacket(ResourcePackStatusC2SPacket.Status.ACCEPTED));
@@ -34,5 +33,9 @@ public class ClientPlayNetworkHandlerMixin {
 
     public void log(String msg) {
         CLIENT.player.sendMessage(Text.of(("[RP Bypass]: " + msg)), false);
+    }
+
+    private static String textCompoundToString(Text textCompound) {
+        return textCompound.toString().split(Pattern.quote("{"))[1].split(Pattern.quote("}"))[0];
     }
 }
--- src/main/java/net/rpbypass/mixin/MultiplayerScreenMixin.java
+++ src/main/java/net/rpbypass/mixin/MultiplayerScreenMixin.java
@@ -3,7 +3,6 @@ package net.rpbypass.mixin;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,37 +19,15 @@ public class MultiplayerScreenMixin extends Screen {
 
     @Inject(at = @At("TAIL"), method = "init")
     public void init(CallbackInfo ci) {
-        ButtonWidget resourcePackBypassButton = new ButtonWidget(535, 310, 100, 20, new LiteralText("RP Bypass: "), (button) -> {
-            if (!resourcePackBypass) {
-                resourcePackBypass = true;
-                button.setMessage(new LiteralText("RP Bypass: ON"));
-            } else {
-                resourcePackBypass = false;
-                button.setMessage(new LiteralText("RP Bypass: OFF"));
-            }
-        });
+        ButtonWidget resourcePackBypassButton = ButtonWidget.builder(Text.of("RP Bypass: " + (resourcePackBypass ? "ON" : "OFF")), (button) -> {
+            resourcePackBypass = !resourcePackBypass;
+            button.setMessage(Text.of("RP Bypass: " + (resourcePackBypass ? "ON" : "OFF")));
+        }).position(width - 165, height - 55).build();
 
-        ButtonWidget forceDenyButton = new ButtonWidget(535, 285, 100, 20, new LiteralText("Force Deny: "), (button) -> {
-            if (!forceDeny) {
-                forceDeny = true;
-                button.setMessage(new LiteralText("Force Deny: ON"));
-            } else {
-                forceDeny = false;
-                button.setMessage(new LiteralText("Force Deny: OFF"));
-            }
-        });
-
-        if (!resourcePackBypass) {
-            resourcePackBypassButton.setMessage(new LiteralText(resourcePackBypassButton.getMessage().asString() + "OFF"));
-        } else {
-            resourcePackBypassButton.setMessage(new LiteralText(resourcePackBypassButton.getMessage().asString() + "ON"));
-        }
-
-        if (!forceDeny) {
-            forceDenyButton.setMessage(new LiteralText(forceDenyButton.getMessage().asString() + "OFF"));
-        } else {
-            forceDenyButton.setMessage(new LiteralText(forceDenyButton.getMessage().asString() + "ON"));
-        }
+        ButtonWidget forceDenyButton = ButtonWidget.builder(Text.of("Force Deny: " + (forceDeny ? "ON" : "OFF")), (button) -> {
+            forceDeny = !forceDeny;
+            button.setMessage(Text.of("Force Deny: " + (forceDeny ? "ON" : "OFF")));
+        }).position(width - 165, height - 25).build();
 
         this.addDrawableChild(resourcePackBypassButton);
         this.addDrawableChild(forceDenyButton);
